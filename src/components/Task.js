dc973cfdc0f0405107df34bd9cda3ec096c67315
Billy Noyes <wbillypn@gmail.com>
2021-06-11 19:22:41 +0100

    started redux code for deleting task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import './Task.css';
+import { useDispatch } from 'react-redux';
+import { setCheck } from '../features/taskSlice';
+import { deleteTask } from '../features/taskSlice';
 
 function Task({ title, description, creator, worker, done, id}) {
+    const dispatch = useDispatch();
 
     const handleCheck = () => {
+        dispatch(setCheck(id));
+    }
 
+    const handleDelete = () => {
+        dispatch(deleteTask(id));
     }
 
     return (
         <div className="task">
             <p className={done && 'task--done'}>{title}</p>
-            <p>{description}</p>
-            <p>{creator}</p>
-            <p>{worker}</p>
+            <p className={done && 'task--done'}>{description}</p>
+            <p className={done && 'task--done'}>{creator}</p>
+            <p className={done && 'task--done'}>{worker}</p>
             <input type="checkbox" onChange={handleCheck} value={done} />
+            <button onClick={handleDelete} className='task__delete'>🗑</button>
         </div>
     )
 }
--- src/features/taskSlice.js
+++ src/features/taskSlice.js
@@ -10,10 +10,20 @@ const taskSlice = createSlice({
     reducers: {
         saveTask: (state, action) => {
             state.taskList.push(action.payload);
+        },
+        setCheck: (state, action) => {
+            state.taskList.map(item => {
+                if (action.payload === item.id) {
+                    item.done = !item.done;
+                }
+            })
+        },
+        deleteTask: (state, action) => {
+            state.taskList.filter(item => item.id !== action.id);
         }
     }
 });
 
-export const { saveTask } = taskSlice.actions
+export const { saveTask, setCheck, deleteTask } = taskSlice.actions
 export const selectTaskList = state => state.tasks.taskList
 export default taskSlice.reducer
