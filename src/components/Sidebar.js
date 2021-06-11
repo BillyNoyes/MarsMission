e57f3aa086449ddf29c571374a49fba774a48fab
Billy Noyes <wbillypn@gmail.com>
2021-06-11 20:26:46 +0100

    added confirm popups to delete buttons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Sidebar.css';
+import { useDispatch } from 'react-redux';
+import { deleteAll } from '../features/taskSlice';
 
 function Sidebar({ taskList }) {
+    const dispatch = useDispatch();
+    
+    const handleDeleteAll = () => {
+        dispatch(deleteAll());
+    }
+
     return (
         <div className="sidebar">
             <p>Current Use</p>
             <p>{taskList.length} {taskList.length === 1 ? 'Task' : "Tasks"}</p>
-            <button className='sidebar__button'>Delete All</button>
+            <button onClick={() => {
+                if (window.confirm("Delete all Tasks?")) {
+                    handleDeleteAll();
+                }}} 
+            className='sidebar__button'>Delete All</button>
         </div>
     )
 }
