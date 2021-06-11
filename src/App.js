588ce78d3fb17582d0661906c6acbae852a7dddc
Billy Noyes <wbillypn@gmail.com>
2021-06-11 18:11:23 +0100

    created basic input and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Input from './components/Input';
+
 function App() {
   return (
-    <div>
-      Entry Point
+    <div className="app">
+      {/* Header */}
+      {/* Image */}
+      {/* Sidebar */}
+      {/* todolist */}
+      <Input />
     </div>
   );
 }
