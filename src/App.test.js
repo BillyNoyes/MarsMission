048b82fd3a15d93b6697bdf32bb1e125ea7f7a2a
Billy Noyes <wbillypn@gmail.com>
2021-06-12 00:07:06 +0100

    started work on tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { render, fireEvent, getByTestId } from '@testing-library/react';
 
+import Sidebar from './components/Sidebar';
+import Input from './components/Input';
 import Image from './components/Image';
-
-// Feel free to use a different test framework you're more familiar with
 
 test('Test Template', () => {  
   // const { getByText } = render(<App />);
@@ -12,10 +12,30 @@ test('Test Template', () => {
   // expect(linkElement).toBeInTheDocument();
 });
 
-describe('Mars Image', () => {
-  test('Image of Mars must be showing from the API', () => {
-    render(<Image />);
-    const Image = screen.getByRole('img');
-    expect(logo).toHaveAttribute('alt', 'Mars');
-  });
-});
+const setup = () => {
+  const utils = render(<Input />)
+  const input = utils.getByPlaceholderText('Task Title');
+  return {
+    input,
+    ...utils,
+  }
+}
+
+test('It should allow text to be entered', () => {
+  const { input } = setup()
+  fireEvent.change(input, { target: { value: 'Walk this direction' } })
+  expect(input.value).toBe('Walk this direction')
+})
+
+
+test("Check Image is Rendered", async () => {
+  render(<Image />);
+  const img = await screen.getByRole('img');
+  expect(img).toHaveAttribute('alt', 'Mars');
+})
+
+test("Check username is displayed", async () => {
+  const { getByTestId } = render(<Sidebar username='testuser' />);
+  const name = getByTestId('username');
+  expect(name.value).toBe('username');
+})
