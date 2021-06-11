bdf10d4f1783d9f5830306ac41397efb1856c29e
Billy Noyes <wbillypn@gmail.com>
2021-06-11 18:55:04 +0100

    added error message for inputs that contain null data

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,24 +4,34 @@ import { useDispatch } from 'react-redux';
 import { saveTask } from '../features/taskSlice';
 
 function Input() {
-    const [input, setInput] = useState('');
-
     const [taskTitle, setTaskTitle] = useState('');
     const [description, setDescription] = useState('');
     const [taskCreator, setTaskCreator] = useState('');
     const [taskWorker, setTaskWorker] = useState('');
+    const [error, setError] = useState('');
+
 
     const dispatch = useDispatch();
 
     const addTask = () => {
-        dispatch(saveTask({
-            title: taskTitle,
-            desc: description,
-            creator: taskCreator,
-            worker: taskWorker,
-            done: false,
-            id: Date.now()
-        }))
+        if (taskTitle != null && description != null && taskCreator != null && taskWorker != null) {
+            dispatch(saveTask({
+                title: taskTitle,
+                desc: description,
+                creator: taskCreator,
+                worker: taskWorker,
+                done: false,
+                id: Date.now()
+            }))
+    
+            setTaskTitle('');
+            setDescription('');
+            setTaskCreator('');
+            setTaskWorker('');
+        } else {
+            setError('Each input must contain text');
+        }
+        
     }
 
     return (
@@ -30,8 +40,8 @@ function Input() {
             <input type="text" placeholder='Description' value={description} onChange={e => setDescription(e.target.value)} />
             <input type="text" placeholder='Creator' value={taskCreator} onChange={e => setTaskCreator(e.target.value)} />
             <input type="text" placeholder='Worker' value={taskWorker} onChange={e => setTaskWorker(e.target.value)} />
-
             <button onClick={addTask}>Add</button>
+            <p>{error}</p>
         </div>
     )
 }
